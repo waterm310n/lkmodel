f755b44e9b040c1195b7072f153cf7dce463c5bc
Shi Lei <shi_lei@massclouds.com>
2024-06-02 07:06:01 +0000

    rt_axconfig: add it

---
+++ axconfig/rt_axconfig/src/main.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "Rust" fn runtime_main(_cpu_id: usize, _dtb_pa: usize) {
+    assert_eq!(axconfig::ARCH, "riscv64");
+    axhal::misc::terminate();
+}
+
+#[panic_handler]
+pub fn panic(info: &PanicInfo) -> ! {
+    arch_boot::panic(info)
+}
