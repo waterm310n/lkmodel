117403bc441afb32ad0298b0ba5a1ebcbdf6d0a5
Shi Lei <shi_lei@massclouds.com>
2024-06-27 17:03:30 +0000

    add 5_4

---
+++ tour_stage/rt_tour_5_4/src/main.rs
@@ -0,0 +1,74 @@
+//! Startup process for monolithic kernel.
+
+#![no_std]
+#![no_main]
+
+#[macro_use]
+extern crate axlog2;
+
+use core::panic::PanicInfo;
+
+const LOGO: &str = r#"
+       d8888                            .d88888b.   .d8888b.
+      d88888                           d88P" "Y88b d88P  Y88b
+     d88P888                           888     888 Y88b.
+    d88P 888 888d888  .d8888b  .d88b.  888     888  "Y888b.
+   d88P  888 888P"   d88P"    d8P  Y8b 888     888     "Y88b.
+  d88P   888 888     888      88888888 888     888       "888
+ d8888888888 888     Y88b.    Y8b.     Y88b. .d88P Y88b  d88P
+d88P     888 888      "Y8888P  "Y8888   "Y88888P"   "Y8888P"
+"#;
+
+/// The main entry point for monolithic kernel startup.
+#[cfg_attr(not(test), no_mangle)]
+pub extern "Rust" fn runtime_main(cpu_id: usize, dtb: usize) {
+    axlog2::init("info");
+    info!("[rt_tour_5_4]: ...");
+    init(cpu_id, dtb);
+    start(cpu_id, dtb);
+    info!("[rt_tour_5_4]: ok!");
+    panic!("Never reach here!");
+}
+
+pub fn init(cpu_id: usize, dtb: usize) {
+    show_logo();
+
+    userboot::init(cpu_id, dtb);
+    axtrap::init(cpu_id, dtb);
+
+    info!(
+        "MacroKernel is starting: Primary CPU {} started, dtb = {:#x}.",
+        cpu_id, dtb
+    );
+}
+
+pub fn start(cpu_id: usize, dtb: usize) {
+    axtrap::start(cpu_id, dtb);
+    userboot::start(cpu_id, dtb);
+}
+
+fn show_logo() {
+    ax_println!("{}", LOGO);
+    ax_println!(
+        "\
+        arch = {}\n\
+        platform = {}\n\
+        target = {}\n\
+        smp = {}\n\
+        build_mode = {}\n\
+        log_level = {}\n\
+        ",
+        option_env!("AX_ARCH").unwrap_or(""),
+        option_env!("AX_PLATFORM").unwrap_or(""),
+        option_env!("AX_TARGET").unwrap_or(""),
+        option_env!("AX_SMP").unwrap_or(""),
+        option_env!("AX_MODE").unwrap_or(""),
+        option_env!("AX_LOG").unwrap_or(""),
+    );
+}
+
+#[panic_handler]
+pub fn panic(info: &PanicInfo) -> ! {
+    error!("{}", info);
+    arch_boot::panic(info)
+}
