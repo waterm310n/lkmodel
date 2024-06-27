8927681c85d6d09d08a84d7e6b1b98bc92f2656e
Shi Lei <shi_lei@massclouds.com>
2024-06-27 08:06:40 +0000

    add 3_1

---
+++ tour_stage/rt_tour_3_1/src/main.rs
@@ -0,0 +1,99 @@
+#![no_std]
+#![no_main]
+#![feature(asm_const)]
+
+#[macro_use]
+extern crate axlog2;
+extern crate alloc;
+
+mod userboot;
+mod trap;
+
+use core::panic::PanicInfo;
+use alloc::sync::Arc;
+use wait_queue::WaitQueue;
+use taskctx::PF_KTHREAD;
+use mutex::Mutex;
+use spinbase::SpinNoIrq;
+use page_table::paging::pgd_alloc;
+use page_table::MappingFlags;
+use axhal::arch::write_page_table_root0;
+use userboot::USER_APP_ENTRY;
+use axhal::mem::PAGE_SIZE_4K;
+
+static WQ: WaitQueue = WaitQueue::new();
+static APP_READY: Mutex<bool> = Mutex::new(false);
+
+#[no_mangle]
+pub extern "Rust" fn runtime_main(cpu_id: usize, dtb_pa: usize) {
+    axlog2::init("debug");
+    info!("[rt_tour_3_1]: ...");
+
+    // Setup simplest trap framework.
+    trap::init();
+
+    // Startup a kernel thread.
+    run_queue::init(cpu_id, dtb_pa);
+    trap::start();
+
+    let ctx = run_queue::spawn_task_raw(1, 0, move || {
+        // Prepare for user app to startup.
+        userboot::init(cpu_id, dtb_pa);
+
+        info!("App kernel-thread load ..");
+        // Load userland app into pgd.
+        userboot::load();
+
+        // Note: wait wander-thread to notify.
+        info!("App kernel-thread waits for wanderer to notify ..");
+        *APP_READY.lock() = true;
+        WQ.wait();
+
+        // Start userland app.
+        info!("App kernel-thread is starting ..");
+        userboot::start();
+        userboot::cleanup();
+    });
+    run_queue::activate_task(ctx.clone());
+
+    let ctx = run_queue::spawn_task_raw(2, PF_KTHREAD, || {
+        info!("Wander kernel-thread is running ..");
+
+        // Alloc new pgd and setup.
+        let pgd = Arc::new(SpinNoIrq::new(pgd_alloc()));
+        unsafe {
+            write_page_table_root0(pgd.lock().root_paddr().into());
+        }
+
+        let mut ctx = taskctx::current_ctx();
+        assert!(ctx.pgd.is_none());
+        ctx.as_ctx_mut().set_mm(2, pgd.clone());
+
+        let flags = MappingFlags::READ | MappingFlags::WRITE | MappingFlags::EXECUTE | MappingFlags::USER;
+        pgd.lock().map_region_and_fill(USER_APP_ENTRY.into(), PAGE_SIZE_4K, flags).unwrap();
+        info!("Wanderer: Map user page: {:#x} ok!", USER_APP_ENTRY);
+
+        // Try to access user app code area
+        let size = 16;
+        let run_code = unsafe { core::slice::from_raw_parts_mut(USER_APP_ENTRY as *mut u8, size) };
+        info!("Try to access app code: {:?}", &run_code[0..size]);
+
+        info!("Wander kernel-thread waits for app to be ready ..");
+        while !(*APP_READY.lock()) {
+            run_queue::yield_now();
+        }
+        info!("Wander notifies app ..");
+        WQ.notify_one(true);
+    });
+    run_queue::activate_task(ctx.clone());
+
+    run_queue::yield_now();
+
+    unreachable!();
+}
+
+#[panic_handler]
+pub fn panic(info: &PanicInfo) -> ! {
+    error!("{:?}", info);
+    arch_boot::panic(info)
+}
---
+++ tour_stage/rt_tour_3_1/src/userboot.rs
@@ -0,0 +1,66 @@
+/// userboot
+///
+/// Start first userland app.
+
+use alloc::sync::Arc;
+use axhal::mem::PAGE_SIZE_4K;
+use axhal::arch::start_thread;
+use axhal::trap::TRAPFRAME_SIZE;
+use page_table::paging::MappingFlags;
+use page_table::paging::pgd_alloc;
+use axalloc::global_allocator;
+use spinbase::SpinNoIrq;
+use axhal::arch::write_page_table_root0;
+
+pub const USER_APP_ENTRY: usize = 0x1000;
+
+pub fn load() {
+    let result = pflash::load_next(None);
+    assert!(result.is_some());
+    let (va, size) = result.unwrap();
+    info!("Got pflash payload: pos {:#x} size {}", va, size);
+    let load_code = unsafe { core::slice::from_raw_parts(va as *const _, size) };
+
+    let ctx = taskctx::current_ctx();
+    let pgd = ctx.try_pgd().expect("Current task has no pgd!");
+
+    let flags = MappingFlags::READ | MappingFlags::WRITE | MappingFlags::EXECUTE | MappingFlags::USER;
+    pgd.lock().map_region_and_fill(USER_APP_ENTRY.into(), PAGE_SIZE_4K, flags).unwrap();
+    info!("Map user page: {:#x} ok!", USER_APP_ENTRY);
+
+    let run_code = unsafe { core::slice::from_raw_parts_mut(USER_APP_ENTRY as *mut u8, size) };
+    run_code.copy_from_slice(load_code);
+
+    info!("App code: {:?}", &run_code[0..size]);
+}
+
+pub fn start() {
+    // Prepare kernel stack
+    let ksp = global_allocator().alloc_pages(1, PAGE_SIZE_4K).unwrap();
+    info!("Alloc page: {:#x}", ksp);
+
+    let pt_regs = ksp + PAGE_SIZE_4K - TRAPFRAME_SIZE;
+    start_thread(pt_regs, USER_APP_ENTRY, 0);
+    axhal::arch::ret_from_fork(pt_regs);
+}
+
+pub fn cleanup() {
+    let ctx = taskctx::current_ctx();
+    let pgd = ctx.try_pgd().expect("Current task has no pgd!");
+    pgd.lock().unmap_region_and_free(USER_APP_ENTRY.into(), PAGE_SIZE_4K).unwrap();
+}
+
+pub fn init(cpu_id: usize, dtb_pa: usize) {
+    pflash::init(cpu_id, dtb_pa);
+
+    // Alloc new pgd and setup.
+    let pgd = Arc::new(SpinNoIrq::new(pgd_alloc()));
+    unsafe {
+        write_page_table_root0(pgd.lock().root_paddr().into());
+    }
+
+    taskctx::init(cpu_id, dtb_pa);
+    let mut ctx = taskctx::current_ctx();
+    assert!(ctx.pgd.is_none());
+    ctx.as_ctx_mut().set_mm(1, pgd);
+}
